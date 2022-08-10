85301aefe499f7716a81abee49e9bc420044467c
Becky75 <alpaca.love5@gmail.com>
2022-08-10 13:40:43 -0700

    updated

--- project/js/one_page_website.js
+++ project/js/one_page_website.js
@@ -33,7 +33,7 @@ function showSlides(n){
     for(i=0;i<dots.length;i++){
         dots[i].className=dots[i].className.replace("ative","");
     }
-    slides[slideIndex-1].style.display="block";
+    slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className+="active";
     captionText.innerHTML=dots[slideIndex-1].alt;
 }
