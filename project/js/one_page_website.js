52405cdae4db7c4fad7834c3d0fc7ee6f1e1823f
Becky75 <alpaca.love5@gmail.com>
2022-08-12 22:48:19 -0700

    updated

--- project/js/one_page_website.js
+++ project/js/one_page_website.js
@@ -37,3 +37,10 @@ function showSlides(n){
     captionText.innerHTML=dots[slideIndex-1].alt;
 }
 
+//this is for a pop-up form
+function openForm(){
+    document.getElementById("myForm").style.display="block";
+}
+function closeForm(){
+    document.getElementById("myForm").style.display="none";
+}
